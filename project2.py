b9e2dd24b74f4e26a935d8c9b3ca4e8f0fe07402
Ishitha10 <ishithabommu1@gmail.com>
2024-10-02 19:14:42 +0100

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,8 @@
+char = input("Enter a character: ")
+def is_alphabet(char):
+    if char.isalpha():
+        print(f"(char)is an alphabet")
+    else:
+        print(f"(char)is not an alphabet")
+       
+        is_alphabet(char)
